f6a435681b93276e9238ecc0da7d8b30812f7abb
s3kken <your.unicorn0@gmail.com>
2022-12-16 20:53:13 +0700

    Initial commit

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('register', views.RegisterUser.as_view(), name='register'),
+]
---
+++ polls/views.py
@@ -0,0 +1,59 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import CreateView
+
+from .forms import Registration
+from .models import Question, Choice, Vote, User
+from django.template import loader
+from django.urls import reverse, reverse_lazy
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+@login_required
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'вы не сделали выбор'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        question.question_votes += 1
+        question.question_votes.save()
+        user_choice = Vote.objects.create(question=question, user=request.user)
+        user_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+class RegisterUser(CreateView):
+    form_class = Registration
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+    succes_page = 'registration'
+
+
+
+
